67bbcccb82008d1fc9337b0d53c9050deeb0658f
L-Karimi <luciahkei101@gmail.com>
2022-05-27 14:18:32 +0300

    OOP

--- student.py
+++ student.py
@@ -1,8 +1,15 @@
 from student import Student
 
-Class Student:
-name = "Lucy"
-country = "Kenya"
-school ="AkiraCix"
-Lucy = Student()
-Lucy.country()
+class Student:  
+    def __init__(self,school,name,country):
+    
+             
+     name = "Lucy"
+     country = "Kenya"
+     school ="AkiraChix"
+     Lucy = Student()
+     Lucy.country()
+     self.country()
+     Lucy = Student(name="Lucy",age=22,country="Kenya",school="AkiraChix")
+     Lucy = Student(name="Pascaline",age=18,country="Rwanda",school="AkiraChix")
+     Lucy = Student(name="Elizabeth", age=23,country="Kenya",school="AkiraChix")
